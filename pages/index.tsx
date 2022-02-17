bb63b5d673f60d4b83e6c853936c696bc60622c0
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-17 12:58:41 +0530

    Data fetcing added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage, GetStaticProps } from "next";
+import Head from "next/head";
+import { apiResponse, Blog } from "../types/index";
+import BlogList from "../components/BlogList";
 
-const Home: NextPage = () => {
+const Home: NextPage<apiResponse> = ({ data, success }) => {
+  const blogs = data as Blog[];
+
   return (
     <div>
       <Head>
@@ -9,11 +13,19 @@ const Home: NextPage = () => {
         <meta name="description" content="Blogging App built in Nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl text-blue-600 font-bold underline">
-        Hello world!
-      </h1>
+
+      {success && <BlogList blogs={blogs} />}
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const blogs = await (
+    await fetch(`https://next-blog-app-six.vercel.app/api/blogs`)
+  ).json();
+  return {
+    props: blogs,
+  };
 };
 
 export default Home;
